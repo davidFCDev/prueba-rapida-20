8b64e168ae872adff6288f143b54f6f78e08d230
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-08 15:20:29 +0200

    useSearch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
+import { useEffect, useId, useState } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import responseMovies from "./mocks/with-results.json";
 
+function useSearch() {
+  const [search, updateSearch] = useState("");
+  const [error, setError] = useState(null);
+  const isFirstInput = useId(true);
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === "";
+      return;
+    }
+    if (search === "") {
+      setError("No se puede buscar una película vacía");
+      return;
+    }
+
+    if (search.length < 3) {
+      setError("La búsqueda debe tener al menos 3 caracteres");
+      return;
+    }
+
+    setError(null);
+  }, [search, isFirstInput]);
+
+  return { search, updateSearch, error };
+}
+
 function App() {
+  const { search, updateSearch, error } = useSearch();
   const movies = responseMovies.Search;
 
   const mappedMovies = movies.map((movie) => {
@@ -22,6 +50,7 @@ function App() {
           <input type="text" placeholder="Escribe aquí tu película" />
           <button type="submit">Buscar</button>
         </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </header>
 
       <main>
