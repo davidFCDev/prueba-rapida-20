340ceccdb6b6aaed09ffc550ee2fb427618fdfea
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-08 15:23:41 +0200

    peticion api

--- src/App.jsx
+++ src/App.jsx
@@ -31,23 +31,28 @@ function useSearch() {
 
 function App() {
   const { search, updateSearch, error } = useSearch();
-  const movies = responseMovies.Search;
 
-  const mappedMovies = movies.map((movie) => {
-    return {
-      id: movie.imdbID,
-      title: movie.Title,
-      year: movie.Year,
-      poster: movie.Poster,
-    };
-  });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Buscando", search);
+  };
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value;
+    updateSearch(newSearch);
+  };
 
   return (
     <div className="page">
       <header>
         <h1>Buscador de películas</h1>
-        <form>
-          <input type="text" placeholder="Escribe aquí tu película" />
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={handleChange}
+            value={search}
+            type="text"
+            placeholder="Escribe aquí tu película"
+          />
           <button type="submit">Buscar</button>
         </form>
         {error && <p style={{ color: "red" }}>{error}</p>}
--- src/services/movies.js
+++ src/services/movies.js
@@ -1,4 +1,23 @@
 export const searchMovies = async ({ search }) => {
   const API_KEY_OMDB = "6c52a547";
   const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY_OMDB}&s=${search}`;
+
+  if (search === "") return null;
+
+  try {
+    const res = await fetch(API_URL);
+    const data = res.json();
+
+    const movies = data.Search;
+    return movies.map((movie) => {
+      return {
+        id: movie.imdbID,
+        title: movie.Title,
+        year: movie.Year,
+        poster: movie.Poster,
+      };
+    });
+  } catch (error) {
+    throw new Error("Error al buscar las películas");
+  }
 };
